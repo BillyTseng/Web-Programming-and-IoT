ecb8e0687b041218d8492d4e07627952b3e9ff85
Billy Tseng <tseng.billy@gmail.com>
2017-09-30 08:22:21 -0700

    refactor: Add better approach to error handling.

--- in-class/0927_json/script.js
+++ in-class/0927_json/script.js
@@ -2,24 +2,27 @@ function parseJSONData() {
   var resultsList = document.getElementById("resultsList");
   var jsonData = document.getElementById("jsonData").value;
   var jsonObj;
+
   try {
       jsonObj = JSON.parse(jsonData);
+      var stringHtml = "";
+      stringHtml += "<ol>";
+
+      for (var i = 0; i < jsonObj.results.length; i++) {
+          stringHtml += "<li>" + jsonObj.results[i].year + ", ";
+          stringHtml += jsonObj.results[i].color + ", ";
+          stringHtml += jsonObj.results[i].make + ", ";
+          stringHtml += jsonObj.results[i].model + "</li>";
+      }
+
+      stringHtml += "</ol>";
+      resultsList.innerHTML = stringHtml;
   } catch (ex) {
-      console.log(ex.name + ": " + ex.message);
-      return;
-  }
-  var stringHtml = "";
-  stringHtml += "<ol>";
-
-  for (var i = 0; i < jsonObj.results.length; i++) {
-      stringHtml += "<li>" + jsonObj.results[i].year + ", ";
-      stringHtml += jsonObj.results[i].color + ", ";
-      stringHtml += jsonObj.results[i].make + ", ";
-      stringHtml += jsonObj.results[i].model + "</li>";
+      var errorMsg = ex.name + ": " + ex.message;
+      console.log(errorMsg);
+      resultsList.innerHTML = '<p>' + errorMsg + '</p>';
   }
 
-  stringHtml += "</ol>";
-  resultsList.innerHTML = stringHtml;
 }
 
 document.getElementById("parseJson").addEventListener("click", parseJSONData);
