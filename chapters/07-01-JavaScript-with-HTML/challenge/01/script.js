22c2045a7d82a68e0d42b6c2364a35e3c6f43484
Billy Tseng <tseng.billy@gmail.com>
2017-10-09 01:39:37 -0700

    feat: JavaScript with HTML

---
+++ chapters/07-01-JavaScript-with-HTML/challenge/01/script.js
@@ -0,0 +1,10 @@
+/* Use the writeln method of the document object to display the current platform
+ in a <p> tag in the webpage. Hint: The platform property of the window.navigator
+  object contains the current platform.*/
+
+<h1>Demo</h1>
+<script>
+
+   <!-- Your solution goes here -->
+   document.writeln("<p>"+window.navigator.platform+"</p>");
+</script>
---
+++ chapters/07-01-JavaScript-with-HTML/challenge/02/script.js
@@ -0,0 +1,15 @@
+/* Add a <script> tag to the header that loads the JavaScript file https://code.jquery.com/jquery-3.2.1.min.js. */
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>JavaScript Demo</title>
+   <meta charset="UTF-8">
+
+   <!-- Your solution goes here -->
+   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+</head>
+<body>
+   <h1>Loading externally</h1>
+</body>
+</html>
