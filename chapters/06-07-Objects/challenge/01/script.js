a161751d6459ca9ec0de751ef9461c762dc1804e
Billy Tseng <tseng.billy@gmail.com>
2017-10-29 14:38:43 -0700

    feat: display the movie's director.

---
+++ chapters/06-07-Objects/challenge/01/script.js
@@ -0,0 +1,17 @@
+/* Display the movie's director. */
+
+var movie = { // Code will be tested with a different movie
+   name: "Interstellar",
+   director: "Christopher Nolan",
+   composer: "Hans Zimmer",
+   cast: {
+      "Matthew McConaughey": "Cooper",
+      "Anne Hathaway": "Brand",
+      "Jessica Chastain": "Murph",
+      "Matt Damon": "Mann",
+      "Mackenzie Foy": "Young Murph"
+   }
+};
+
+/* Your solution goes here */
+console.log(movie.director);
