52846e19b6de9c34aa24cd692dcf19ad0d3d4720
Billy Tseng <tseng.billy@gmail.com>
2017-11-04 09:42:30 -0700

    feat: define a new object in variable band.

---
+++ chapters/06-07-Objects/challenge/04/script.js
@@ -0,0 +1,11 @@
+/* Define a new object in variable band that has properties "name" storing
+ "Dire Straits"; "albums" storing "Brothers in Arms", "Love over Gold" and
+  "On Every Street"; and "members" storing "Mark Knopfler", "David Knopfler",
+   "John Illsley" and "Pick Withers". */
+
+/* Your solution goes here */
+var band = {
+  name: "Dire Straits",
+  albums: ["Brothers in Arms", "Love over Gold", "On Every Street"],
+  members: ["Mark Knopfler", "David Knopfler", "John Illsley", "Pick Withers"]
+};
